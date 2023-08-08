f52c01f5eb1fb1ca7f353ee8896ee6f6f35c7170
enrique-villarrealp <85933898+enrique-villarrealp@users.noreply.github.com>
2023-08-08 21:44:41 +0200

    feat: Add NormalFloat colors

--- nvim/lua/lazyvim/plugins/catpuccin.lua
+++ nvim/lua/lazyvim/plugins/catpuccin.lua
@@ -4,6 +4,22 @@ return {
   priority = 1000,
   ---@class CatppuccinOptions
   opts = {
+    flavour = "mocha",
     transparent_background = true,
+    integrations = {
+      noice = true,
+      notify = true,
+      barbecue = {
+        dim_dirname = true,
+        bold_basename = true,
+        dim_context = false,
+        alt_background = false,
+      },
+    },
+    custom_highlights = function(color)
+      return {
+        NormalFloat = { fg = "#FFFFFF", bg = "#222222" },
+      }
+    end,
   },
 }

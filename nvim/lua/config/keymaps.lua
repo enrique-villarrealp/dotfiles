0ba96cc531b800256be14490c7e9994bbc93d582
enrique-villarrealp <85933898+enrique-villarrealp@users.noreply.github.com>
2024-03-10 11:18:34 +0100

    refactor: change keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -28,12 +28,15 @@ keymap.set("n", "<leader>fF", function() builtin.find_files({ cwd = utils.buffer
 
 -- Search
 keymap.set("n", "<leader>sg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>", { desc = "Grep" })
-keymap.set("n", "<leader>gG", function() builtin.live_grep({ cwd = utils.buffer_dir() }) end, { desc = "Grep (cwd)" })
+keymap.set("n", "<leader>sG", function() builtin.live_grep({ cwd = utils.buffer_dir() }) end, { desc = "Grep (cwd)" })
 keymap.set("n", "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Find in Buffer" })
 keymap.set("n", "<leader>sr", function() require("spectre").open() end, { desc = "Replace in files (spectre)" })
+keymap.set("n", "<leader>sr", function() require("spectre").open() end, { desc = "Replace in files (spectre)" })
+keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {desc = "Search current word"})
 
 -- Git
 keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", { desc = "Branches" })
+
 keymap.set("n", "<leader>gs", "<cmd>Neogit<cr>", { desc = "Neogit Status" })
 keymap.set("n", "<leader>gp", "<cmd>Neogit pull<cr>", { desc = "Neogit Pull" })
 keymap.set("n", "<leader>gP", "<cmd>Neogit push<cr>", { desc = "Neogit Push" })
@@ -59,18 +62,14 @@ keymap.set("n", "<C-t>", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", { d
 keymap.set("n", "<leader>fS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", { desc = "Find symbols in workspace" })
 
 -- Harpoon
-keymap.set("n", "<leader>'", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Add to Harpoon" })
+keymap.set("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Add to Harpoon" })
 -- keymap.set("n", "<leader>0", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { desc = "Show Harpoon" })
-keymap.set("n", "<leader>0", "<cmd>Telescope harpoon marks<cr>", { desc = "Show Harpoon" })
+keymap.set("n", "<leader>ho", "<cmd>Telescope harpoon marks<cr>", { desc = "Show Harpoon" })
 keymap.set("n", "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", { desc = "Harpoon Buffer 1" })
 keymap.set("n", "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", { desc = "Harpoon Buffer 2" })
 keymap.set("n", "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", { desc = "Harpoon Buffer 3" })
 keymap.set("n", "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", { desc = "Harpoon Buffer 4" })
 keymap.set("n", "<leader>5", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", { desc = "Harpoon Buffer 5" })
-keymap.set("n", "<leader>6", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", { desc = "Harpoon Buffer 6" })
-keymap.set("n", "<leader>7", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", { desc = "Harpoon Buffer 7" })
-keymap.set("n", "<leader>8", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", { desc = "Harpoon Buffer 8" })
-keymap.set("n", "<leader>9", "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", { desc = "Harpoon Buffer 9" })
 
 -- Zen Mode
 keymap.set("n", "<leader>z", "<cmd>ZenMode<cr>", { desc = "Toggle Zen Mode" })

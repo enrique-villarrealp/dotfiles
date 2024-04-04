02f08f63a5a88817583a4c0d6f88f55f9bee1914
Enrique Villarreal <enrique.villarreal@glovoapp.com>
2024-04-04 22:33:16 +0200

    feat: change theme to catpuccin again
    still pending to change it for alacritty

--- nvim/lua/plugins/alabaster.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "p00f/alabaster.nvim" ,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd('colorscheme alabaster')
-      vim.api.nvim_set_hl(0, 'FlashLabel', {bg="#c33c33"})
-    end
-}

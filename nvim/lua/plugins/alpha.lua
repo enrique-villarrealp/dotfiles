35529ebc8dfdc3528a85b27408352524c2f5b148
Enrique Villarreal <enrique.villarreal@glovoapp.com>
2024-04-07 20:49:58 +0200

    feat: update dashboard

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -18,6 +18,16 @@ Do the hard work, especially when you don't feel like it
       ]]
 
       dashboard.section.header.val =  vim.split(logo, "\n")
+      dashboard.section.buttons.val = {
+        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+        dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+        dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/ <CR>"),
+        dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+      }
+
       alpha.setup(dashboard.opts)
       
   end

1904d75eed8edef22718c7eaaebbb0dbcd548ce9
Enrique Villarreal <enrique.villarreal@glovoapp.com>
2024-05-12 11:05:58 +0200

    feat: update nvim-incline

--- nvim/lua/plugins/nvim-incline.lua
+++ nvim/lua/plugins/nvim-incline.lua
@@ -2,66 +2,38 @@ return {
   "b0o/incline.nvim",
   lazy=false,
   config = function()
+
     require('incline').setup {
-      debounce_threshold = {
-        falling = 50,
-        rising = 10
+          window = {
+          padding = 0,
+          margin = { horizontal = 0, vertical = 0 },
       },
-      hide = {
-        cursorline = false,
-        focused_win = false,
-        only_win = false
-      },
-      highlight = {
-        groups = {
-          InclineNormal = {
-            default = true,
-            group = "NormalFloat"
-          },
-          InclineNormalNC = {
-            default = true,
-            group = "NormalFloat"
+
+      render = function(props)
+
+          local devicons = require('nvim-web-devicons')
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+          if filename == '' then
+              filename = '[No Name]'
+          end
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+          local modified = vim.bo[props.buf].modified
+
+
+        -- First color: Active BG
+        -- Second color: Inactive BG
+        -- #8f8741
+          local bgcol = props.focused and '#89B4FA' or '#1E1E2E'
+          local res = {
+              ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = '#000000' } or '',
+              ' ',
+              { filename, gui = modified and 'bold,italic' or 'bold' },
+              guibg = bgcol,
+              guifg = props.focused and '#000000' or '#cdd6f4',
           }
-        }
-      },
-      ignore = {
-        buftypes = "special",
-        filetypes = {},
-        floating_wins = true,
-        unlisted_buffers = true,
-        wintypes = "special"
-      },
-      render = "basic",
-      window = {
-        margin = {
-          horizontal = 1,
-          vertical = 1
-        },
-        options = {
-          signcolumn = "no",
-          wrap = false
-        },
-        padding = 1,
-        padding_char = " ",
-        placement = {
-          horizontal = "right",
-          vertical = "top"
-        },
-        width = "fit",
-        winhighlight = {
-          active = {
-            EndOfBuffer = "None",
-            Normal = "InclineNormal",
-            Search = "None"
-          },
-          inactive = {
-            EndOfBuffer = "None",
-            Normal = "InclineNormalNC",
-            Search = "None"
-          }
-        },
-        zindex = 50
-    }
+          table.insert(res, ' ')
+          return res
+      end
   }
   end
 }
